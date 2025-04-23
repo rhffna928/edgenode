9d1a224845a46a8fc894ab3289c1fe95a713e953
rhffna928 <rhffna928@naver.com>
2025-04-23 15:29:23 +0900

    config setting

--- pj_1(confluent)/soc_250410sky.py
+++ pj_1(confluent)/soc_250410sky.py
@@ -41,15 +41,6 @@ file_encoding = 'utf-8'
 send_encoding = 'CP949'
 recv_encoding = 'CP949'
 
-KAFKA_BROKER = os.getenv("KAFKA_BROKER", "192.168.10.101:9092")
-
-producer_config = {'bootstrap.servers': KAFKA_BROKER}
-producer = Producer(producer_config)
-
-consumer_config = {'bootstrap.servers': KAFKA_BROKER, 'group.id': random.randint(0, 100), 'auto.offset.reset': 'latest'}
-consumer = Consumer(consumer_config)
-consumer.subscribe(['special'])
-
 # jpype.startJVM()
 # jpype.addClassPath("watosysEncrypt_not_otp_v1.0.0.jar")
 # jvm_msg_encrypt_class = jpype.JClass("watosys.utils.eg.msg.MsgEncrypt")
@@ -393,6 +384,8 @@ if __name__ == "__main__":
     _pub_init_topic = _config['MQTT']['PUB_INIT_TOPIC']
     _pub_edgenode_topic = _config['MQTT']['PUB_EDGENODE_TOPIC']
 
+    _kafka_broker = _config['KAFKA']['KAFKA_BROKER']
+    _kafka_port = _config['KAFKA']['KAFKA_PORT']
     #_edgeid = _config['APP']['EDGEID'] 
     #_edgety = _config['APP']['EDGETY'] 
     
@@ -400,8 +393,17 @@ if __name__ == "__main__":
     create_rotating_log(full_path, _config['LOGGER'])
     
     HOST1, PORT1 = _host1, _port1
+    
+    KAFKA_BROKER = f'{_kafka_broker}:{_kafka_port}'
+
+    producer_config = {'bootstrap.servers': KAFKA_BROKER}
+    producer = Producer(producer_config)
+
+    consumer_config = {'bootstrap.servers': KAFKA_BROKER, 'group.id': random.randint(0, 100), 'auto.offset.reset': 'latest'}
+    consumer = Consumer(consumer_config)
+    consumer.subscribe(['special'])
+    
     try:
-        
         server1 = ThreadedTCPRequestHandler((socket.gethostbyname(HOST1), PORT1), MyTCPHandler1)
     except ValueError:
         sys.exit()
