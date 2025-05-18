dcc5598ea8d85749934f758b33e4363c786651dd
rhffna928 <rhffna928@naver.com>
2025-05-19 08:57:28 +0900

    timestamp 수정

--- pj_1(confluent)/vehicle(4.21).py
+++ pj_1(confluent)/vehicle(4.21).py
@@ -101,7 +101,7 @@ class VehicleDataConsumer:
     def data_merge(self):
         """스레드 실행메소드"""
         while(True):
-            time.sleep(2)
+            time.sleep(1)
             now = datetime.datetime.now()
 
             # 공통정보, 위치 머지한값 보내지
